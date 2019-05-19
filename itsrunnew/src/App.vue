4bf1eb605b04d3fabccfea27cf65a9da39d99b62
subaru44k <odsubaru@gmail.com>
2019-05-19 11:57:12 -0700

    Delete unused icons

--- itsrunnew/src/App.vue
+++ itsrunnew/src/App.vue
@@ -181,20 +181,8 @@
       >
         <v-card-title class="teal">
           <strong class="subheading">{{ $t("footer_1") }}<a href="https://twitter.com/itsrun_page">{{ $t("footer_2") }}</a>{{ $t("footer_3") }}</strong>
-    
-          <v-spacer></v-spacer>
-    
-          <v-btn
-            v-for="icon in icons"
-            :key="icon"
-            class="mx-3"
-            dark
-            icon
-          >
-            <v-icon size="24px">{{ icon }}</v-icon>
-          </v-btn>
         </v-card-title>
-    
+
         <v-card-actions class="grey darken-3 justify-center">
           &copy;2019 — <strong>{{ $t("title") }}</strong>
         </v-card-actions>

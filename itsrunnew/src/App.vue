2feb3a7e86a0e6b1da789961901e30b405a5ece9
subaru44k <odsubaru@gmail.com>
2019-05-19 15:26:46 -0700

    Delete unused resources

--- itsrunnew/src/App.vue
+++ itsrunnew/src/App.vue
@@ -193,11 +193,9 @@
 
 <script lang="ts">
 import { Component, Vue } from 'vue-property-decorator';
-import HelloWorld from './components/HelloWorld.vue';
 
 @Component({
   components: {
-    HelloWorld,
   },
 })
 export default class App extends Vue {
